af4e892488002888ffb828471e1ce98d629c861c
Dennis Tijbosch <d.tijbosch@yahoo.com>
2018-02-10 16:09:25 +0100

    Initial Commit

--- ToDoList/FirstViewController.swift
+++ ToDoList/FirstViewController.swift
@@ -8,8 +8,36 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+var list = ["Buy milk", "Run 5 miles", "Get Peter", "Plant my new plants"]
 
+class FirstViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    
+    
+    @IBOutlet weak var MyTableView: UITableView!
+    
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return (list.count)
+    }
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "cell")
+        cell.textLabel?.text = list[indexPath.row]
+        return(cell)
+    }
+    
+    public func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.delete
+        {
+            list.remove(at: indexPath.row)
+            MyTableView.reloadData()
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        MyTableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
